e9caa86ac271182ef0350669056bb5303b33243d
Murilo Zampronio <murilo.zamproni@gmail.com>
2025-03-31 15:51:37 -0300

    Add files via upload

---
+++ Loja/Course/src/application/Progam.java
@@ -0,0 +1,46 @@
+package application;
+
+import Entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Progam {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+        System.out.println("Name: ");
+        String name = sc.nextLine();
+        System.out.println("Price: ");
+        double price = sc.nextDouble();
+
+        Product product = new Product(name, price);
+
+        product.setName("Computer");
+        System.out.println("Update name: " + product.getName());
+        product.setPrice(1200.00);
+        System.out.println("Update price: " + product.getPrice());
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+
+        System.out.println("Updated data: " + product);
+
+        sc.close();
+    }
+}
